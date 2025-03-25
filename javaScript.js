d2063bbce819162a56da6a29cfa5ea01bbb4b86c
Dr Wwangondu <74863499+yettus-git@users.noreply.github.com>
2025-03-25 19:33:09 +0300

    Add files via upload

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,6 @@
 function changeText(){
-    document.getElementById("header").innerHTML = "We hope you have a good time!";
-    document.getElementById("header").style.color = "pink";
-    document.getElementById("header").style.fontSize = "50px";
+    document.getElementById("header").innerHTML = "Wanjiku Wang'ondu";
+    document.getElementById("header").style.color = "red";
+    document.getElementById("header").style.fontSize = "20px";
+    document.getElementById("header").style.border = "2px solid black";
 }
