bf93fe8682c4cf1165abc3030044031f94e6cb49
Dr Wwangondu <74863499+yettus-git@users.noreply.github.com>
2025-03-25 19:28:36 +0300

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,5 @@
+function changeTxt(){
+    document.getElementById("header").innerHTML = "We hope you have a good time!";
+    document.getElementById("header").style.color = "pink";
+    document.getElementById("header").style.fontSize = "50px"
+}
